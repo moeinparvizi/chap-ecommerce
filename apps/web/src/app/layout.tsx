01ecb2df04279652e2483ea43b94fbd669a2a7c6
moeinparvizi <giventulip@gmail.com>
2026-07-19 14:41:09 +0330

    fix: add suppressHydrationWarning to body to fix browser extension hydration error

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400&family=Vazirmatn:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </head>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
